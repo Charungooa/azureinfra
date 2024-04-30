efee6f99b73faa04536f403817c0a2f1d99ea64a
v-2amitc <v-2amitc@hotmail.com>
2024-04-30 16:30:17 +0530

    updated the vnet.tf file

--- vnet.tf
+++ vnet.tf
@@ -4,30 +4,29 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "existing_rgp" {
-  name = var.resource-group
+  name = "${var.resource-group}"
+  
 }
 
 resource "azurerm_virtual_network" "vnet2" {
   name                = "${var.vnetname}-vnet"
-  location            = "${data.azurerm_resource_group.existing_rgp.location}"
-  resource_group_name = "${data.azurerm_resource_group.existing_rgp.name}"
-  address_space       = ["${var.address-space}"]
+  location            = data.azurerm_resource_group.existing_rgp.location
+  resource_group_name = data.azurerm_resource_group.existing_rgp.name
+  address_space       = var.address-space
   # tags                = merge(var.tags, map("Name", format("%s-%s", "VNet", var.name)))
 }
 
 resource "azurerm_subnet" "subnet-vnet2" {
-  for_each             = var.subnets
-  name                 = each.key
-  virtual_network_name = "${azurerm_virtual_network.vnet2.name}"
-  resource_group_name  = "${data.azurerm_resource_group.existing_rgp.name}
-  address_prefix       = each.value.addressPrefix
-b}
+   virtual_network_name =   azurerm_virtual_network.vnet2.name
+   address_prefixes =     ["10.0.0.0/24"]  
+   name = "${var.vnetname}-subnet"
+   resource_group_name =   data.azurerm_resource_group.existing_rgp.name  
+}
 
 resource "azurerm_network_security_group" "terraform_security" {
   name                = "terraform_nsg"
-  location            = "${data.azurerm_resource_group.existing_rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.existing_rg.name}"
-  #resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.existing_rgp.location
+  resource_group_name = data.azurerm_resource_group.existing_rgp.name
 
   security_rule {
     name                       = "test123"
@@ -63,19 +62,4 @@ resource "azurerm_network_security_group" "terraform_security" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-
 }
-
-# resource "azurerm_subnet_network_security_group_association" "example" {
-#   subnet_id                 = "${azurerm_subnet.terrasubnet.id}"
-#   network_security_group_id = "${azurerm_network_security_group.example.id}"
-# }
-
-# resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
-#     network_interface_id = "${azurerm_network_interface.azterravnic.id}"
-#     network_security_group_id = "${azurerm_network_security_group.terraform_security.id}"
-# }
-
-
-
