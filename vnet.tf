e9007ecc0606007aef795fc7a0df52abd5f724fb
Charungooa <v-2amitc@hotmail.com>
2024-06-26 07:57:30 +0000

    updated the vnet.tf file

--- vnet.tf
+++ vnet.tf
@@ -11,8 +11,8 @@ resource "azurerm_resource_group" "existing_rgp" {
 # this is the resource block
 resource "azurerm_virtual_network" "vnet2" {
   name                = "${var.vnetname}-vnet"
-  location            = data.azurerm_resource_group.existing_rgp.location
-  resource_group_name = data.azurerm_resource_group.existing_rgp.name
+  location            = azurerm_resource_group.existing_rgp.location
+  resource_group_name = azurerm_resource_group.existing_rgp.name
   address_space       = var.address-space
   # tags                = merge(var.tags, map("Name", format("%s-%s", "VNet", var.name)))
 }
